b260658b2e0bef572373bd1a7d4d0b6018a9a414
Timofey <56739774+haselius@users.noreply.github.com>
2023-07-02 17:53:16 +0300

    Add files via upload

---
+++ lab4/lab4/lab4.2/lab4.2.cpp
@@ -0,0 +1,217 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#define PI 3.14159265
+using namespace std;
+
+//double Answer(double x) 
+//{
+//    return -1.0*tan(x);
+//}
+//double F(double x, double y,double z) 
+//{
+//   return 2.0 * (1.0 + tan(x ) * tan(x )) * y;
+//}
+
+double F(double x, double y, double z)
+{
+    return -1*((2-x)*z+y)/(2*x*(x+2));
+}
+double q(double x) 
+{
+    return -2.0 * (1.0 + tan(x) * tan(x));
+}
+double Answer(double x)
+{
+    return sqrt(abs(x))+x-2;
+}
+void Runge(vector<double> X, vector<double>& Y, vector<double>& Z, double h, double y0, double z0, bool out)
+{
+    vector <double> K1(X.size()), K2(X.size()), K3(X.size()), K4(X.size()), L1(X.size()), L2(X.size()), L3(X.size()), L4(X.size());
+
+    Y[0] = y0;
+    Z[0] = z0;
+    for (int i = 0; i < X.size() - 1; i++)
+    {
+        K1[i] = h * Z[i];
+        L1[i] = h * F(X[i], Y[i], Z[i]);
+
+        K2[i] = h * (Z[i] + 0.5 * L1[i]);
+        L2[i] = h * F((X[i] + 0.5 * h), (Y[i] + 0.5 * K1[i]), (Z[i] + 0.5 * L1[i]));
+
+        K3[i] = h * (Z[i] + 0.5 * L2[i]);
+        L3[i] = h * F((X[i] + 0.5 * h), (Y[i] + 0.5 * K2[i]), (Z[i] + 0.5 * L2[i]));
+
+        K4[i] = h * (Z[i] + 0.5 * L3[i]);
+        L4[i] = h * F(X[i] + 0.5 * h, Y[i] + 0.5 * K3[i], Z[i] + 0.5 * L3[i]);
+
+        Z[i + 1] = Z[i] + (L1[i] + 2.0 * L2[i] + 2.0 * L3[i] + L4[i]) / 6.0;
+        Y[i + 1] = Y[i] + (K1[i] + 2.0 * K2[i] + 2.0 * K3[i] + K4[i]) / 6.0;
+    }
+    if (out == 1)
+    {
+        cout << "Метод Рунге-Кутты: ";
+        for (int k = 0; k < X.size(); k++)
+        {
+            cout << Y[k] << ", ";
+        }
+        cout << endl;
+        cout << "Аналитическое решение: ";
+        for (int k = 0; k < X.size(); k++)
+        {
+            cout << Answer(X[k]) << ", ";
+        }
+        cout << endl;
+        cout << "Погрешность: ";
+        for (int j = 1; j < X.size() - 1; j++)
+        {
+            cout << abs((K2[j] - K3[j])) /abs( (K1[j] - K2[j])) << ", ";
+        }
+        cout << endl;
+        cout << "\n" << endl;
+    }
+}
+void Shoot(vector<double> X, vector<double>& Y, vector<double>& Z, double h, double y0,double y1, vector<double> ksi,double epsilon)
+{
+    vector<double> Phi;
+    int j = 0;
+    Runge(X, Y, Z, h, y0, ksi[0], 0);
+    Phi.push_back(Y[Y.size() - 1]);
+    do {
+
+        Runge(X, Y, Z, h, y0, ksi[j+1], 0);
+        Phi.push_back(Y[Y.size() - 1]);
+        ksi.push_back(ksi[j + 1] - (Phi[j + 1] - y1) * (ksi[j + 1] - ksi[j]) / (Phi[j + 1] - Phi[j]));
+        j++;
+    } while (abs(Phi[Phi.size()-1] - y1) > epsilon);
+    cout << "ksi:" << endl;
+    for (int i = 0; i < ksi.size(); i++)
+    {
+        cout << ksi[i] << ", ";
+    }
+    cout << endl;
+    cout << "abs(Phi):" << endl;
+    for (int i = 0; i < Phi.size(); i++)
+    {
+        cout << abs(Phi[i] - y1) << ", ";
+    }
+    cout << endl;
+    cout << "Метод пристрелки:" << endl;
+    for (int i = 0; i < Y.size(); i++)
+    {
+        cout << Y[i] << ", ";
+    }
+    cout << endl;
+    cout << "X:" << endl;
+    for (int i = 0; i < Y.size(); i++)
+    {
+        cout << X[i] << ", ";
+    }
+    cout << endl;
+    
+    cout << "Погрешность:" << endl;
+    for (int i = 0; i < X.size(); i++)
+    {
+        cout <<Y[i] - Answer(X[i]) << ", ";
+    }
+    cout << endl;
+}
+
+
+void Progonka(vector <double>& X, vector <double> A, vector <double> B, vector <double> C, vector <double> f, int n,double x0,double x1)
+{
+    vector <double> alpha, beta;
+    X[0] = x0;
+    X[n-1] = x1;
+    alpha.push_back(-C[0]/B[0]);
+    beta.push_back(f[0]/B[0]);
+    for (int i = 1; i < n - 2; i++)
+    {
+        alpha.push_back(-C[i] / (B[i] * alpha[i-1] + A[i]));
+        beta.push_back((f[i] - A[i] * beta[i-1]) / (B[i] * alpha[i - 1] + A[i]));
+    }
+    alpha.push_back(0);
+    beta.push_back((f[n-2]-A[n-2]*beta[n-3])/(B[n-2] * alpha[n - 3] + A[n-2]));
+
+    for (int j = n-2; j >= 0; j--)
+    {
+        X[j] = X[j + 1] * alpha[j] + beta[j];
+    }
+    
+}
+
+
+void Kon(vector <double>& X, vector <double>& Y, double h,double p,int N,double y0,double y1)
+{
+    for (int j = 0; j < X.size(); j++)
+    {
+        if (abs(-2 + h * h * q(X[j])) < abs(1 - p * h / 2) + abs(1 + p * h / 2))
+        {
+            cout << "Условие преобладания диоганальных элементов не выполнено" << endl;
+            exit;
+        }
+    }
+    cout << "Условие преобладания диоганальных элементов  выполнено" << endl;
+    vector <double> A = {0, 1.0 / (h * h),1.0 / (h * h) ,1.0 / (h * h)};
+    vector <double> B;
+    vector <double> C = {1.0 / (h * h) ,1.0 / (h * h) ,1.0 / (h * h),0 };
+    for (int i = 1; i < N; i++) 
+    {
+        B.push_back(q(X[i])-2/(h*h));
+    }
+    vector <double> g = { 0.0,0.0,0.0,-y1 / (h * h) };
+    Progonka(Y, A, B, C, g, N, y0, y1);
+    cout << "Конечно-разностный метод:" << endl;
+    for (int k = 0; k < Y.size(); k++)
+    {
+        cout << Y[k] << ", ";
+    }
+    for (int k = 0; k< Y.size(); k++)
+    {
+        cout << Y[k] << ", ";
+    }
+    cout << endl;
+    cout << "Погрешность:" << endl;
+    for (int i = 0; i < X.size(); i++)
+    {
+        cout << Y[i] - Answer(X[i]) << ", ";
+    }
+    cout << endl;
+}
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    //double y0 = 0.0;
+    //double y1 = -sqrt(3.0) / 3.0;
+    double y0 = Answer(0);
+    double y1 = Answer(4.0);
+    double x0 = 0.0;
+    //double x1 = PI / 6.0;
+    double x1 = 4;
+
+    double h = PI / 24.0;
+    
+    vector<double> X;
+    for (double i = x0; i <= x1; i = i + h)
+    {
+        X.push_back(i);
+    }
+    vector<double> Y(X.size()),Z(X.size());
+    vector<double> ksi = { 0,-sqrt(3.0) / 3.0 };
+    Shoot(X, Y, Z, h, y0, y1, ksi, 0.0001);
+    
+
+    double N = X.size();
+    double p = 0.0;
+    double f = 0.0;
+    cout <<"\n" << endl;
+    Kon(X, Y, h, p, N, y0, y1);
+    cout << "Аналитическое решение:" << endl;
+    for (int k = 0; k < X.size(); k++)
+    {
+        cout << Answer(X[k]) << ", ";
+    }
+    cout << endl;
+    return 0;
+}
---
+++ lab4/lab4/lab4/lab4.cpp
@@ -0,0 +1,246 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+double Answer(double x) 
+{
+    return (1 + x) * exp(-x*x);
+}
+double F(double x,double y, double z) 
+{
+    return -4 * x * z -(4 * x*x + 2) * y;
+}
+
+void Eiler(bool key, vector<double>& Y,vector<double> X, double h,double y0,double z0, bool out)
+{
+    if (key == 0)
+    {
+        vector<double> Z(X.size());
+        Y[0] = y0;
+        Z[0] = z0;
+        for (int i = 0; i < X.size()-1; i++) 
+        {
+            Z[i + 1] = Z[i] + h * F(X[i], Y[i], Z[i]);
+            Y[i + 1] = Y[i] + h * Z[i];
+        }
+        if (out == 1)
+        {
+            cout << "Явный метод Эйлера: ";
+            for (int k = 0; k < X.size(); k++)
+            {
+                cout << Y[k] << ", ";
+            }
+            cout << endl;
+            cout << "Аналитическое решение: ";
+            for (int k = 0; k < X.size(); k++)
+            {
+                cout << Answer(X[k]) << ", ";
+            }
+            cout << endl;
+            cout << "Погрешность: ";
+            for (int j = 1; j < X.size(); j++)
+            {
+                double psi = (X[j] - X[j - 1]) / 2.0;
+                cout << sqrt(h) * F(psi, Y[j], Z[j]) / 2.0 << ", ";
+            }
+            cout << endl;
+            cout << "\n" << endl;
+        }
+    }
+    else 
+    {
+        vector<double> Z(X.size()), Zk(X.size());
+        vector<double> Yk(X.size());
+        Y[0] = y0;
+        Z[0] = z0; 
+        Zk[0] = z0;
+        Yk[0] = y0;
+        for (int i = 0; i < X.size() - 1; i++)
+        {
+                Zk[i + 1] = Z[i] + h * F(X[i], Y[i], Z[i]);
+                Yk[i + 1] = Y[i] + h * Z[i];
+                Z[i + 1] = Z[i] + (h/2.0) * (F(X[i], Y[i], Z[i])+ F(X[i+1], Yk[i+1], Zk[i+1]));
+                Y[i + 1] = Y[i] + h *Z[i];
+        }
+        if (out == 1)
+        {
+            cout << "Неявный метод Эйлера: ";
+            for (int k = 0; k < X.size(); k ++)
+            {
+                cout << Y[k] << ", ";
+            }
+            cout << endl;
+            cout << "Аналитическое решение: ";
+            for (int k = 0; k < X.size(); k++)
+            {
+                cout << Answer(X[k]) << ", ";
+            }
+            cout << endl;
+            cout << "Погрешность: ";
+            for (int j = 1; j < X.size(); j++)
+            {
+                double psi = (X[j] - X[j - 1]) / 2;
+                cout << sqrt(h) * F(psi, Y[j], Z[j]) / 2 << ", ";
+            }
+            cout << endl;
+            cout << "\n" << endl;
+        }
+    }
+}
+
+void Runge(vector<double> X, vector<double>& Y, vector<double>& Z, double h, double y0, double z0,bool out,int n)
+{
+    vector <double> K1(n),K2(n),K3(n),K4(n),L1(n),L2(n),L3(n),L4(n);
+
+    Y[0] = y0;
+    Z[0] = z0;
+    for (int i = 0; i < n-1; i++) 
+    {
+        K1[i]=h* Z[i];
+        L1[i]=h* F(X[i], Y[i], Z[i]);
+
+        K2[i] = h * (Z[i] + 0.5 * L1[i]);
+        L2[i]=h* F((X[i] + 0.5 * h), (Y[i] + 0.5 * K1[i]),(Z[i] + 0.5 * L1[i]));
+        
+        K3[i] = h * (Z[i] + 0.5 * L2[i]);
+        L3[i] = h* F((X[i] + 0.5 * h),( Y[i] + 0.5 * K2[i]),( Z[i] + 0.5 * L2[i]));
+        
+        K4[i] = h * (Z[i] + 0.5 * L3[i]);
+        L4[i]=h* F(X[i] + 0.5 * h, Y[i] + 0.5 * K3[i], Z[i] + 0.5 * L3[i]);
+        
+        Z[i + 1] = Z[i] + (L1[i] + 2.0 * L2[i] + 2.0 * L3[i] + L4[i])/6.0;
+        Y[i + 1] = Y[i] + (K1[i] + 2.0 * K2[i] + 2.0 * K3[i] + K4[i])/6.0;
+    }
+    if (out == 1)
+    {
+        cout << "Метод Рунге-Кутты: ";
+        for (int k = 0; k < X.size(); k++)
+        {
+            cout << Y[k] << ", ";
+        }
+        cout << endl;
+        cout << "Аналитическое решение: ";
+        for (int k = 0; k < X.size(); k++)
+        {
+            cout << Answer(X[k]) << ", ";
+        }
+        cout << endl;
+        cout << "Погрешность: ";
+        for (int j = 1; j < X.size() - 1; j++)
+        {
+            cout << abs((K2[j] - K3[j]) / (K1[j] - K2[j])) << ", ";
+        }
+        cout << endl;
+        cout << "\n" << endl;
+    }
+}
+
+void Adams(vector<double> X, vector<double>& Y, vector<double>& Z, double h, double y0, double z0, bool out)
+{
+    Runge(X, Y, Z, h, y0, z0,0,4);
+    for (int i = 3; i < X.size()-1; i++) 
+    {
+        Z[i + 1] = Z[i] + h * (55.0 * F(X[i], Y[i], Z[i]) - 59.0 * F(X[i - 1], Y[i - 1], Z[i - 1]) + 37.0 * F(X[i - 2], Y[i - 2], Z[i - 2]) - 9.0 * F(X[i - 3], Y[i - 3], Z[i - 3]))/24.0;
+    }
+    for (int j = 3; j < X.size()-1; j++) 
+    {
+        Y[j + 1] = Y[j] + h * (55.0 * Z[j] - 59.0 * Z[j-1] + 37.0 * Z[j-2] - 9.0 * Z[j-3])/ 24.0;
+    }
+    if (out == 1)
+    {
+        cout << "Метод Aдамса: ";
+        for (int k = 0; k < X.size(); k++)
+        {
+            cout << Y[k] << ", ";
+        }
+        cout << endl;
+        cout << "Аналитическое решение: ";
+        for (int k = 0; k < X.size(); k++)
+        {
+            cout << Answer(X[k]) << ", ";
+        }
+        cout << endl;
+        cout << "Погрешность: ";
+        for (int j = 1; j < X.size() - 1; j++)
+        {
+            cout << Y[j] - Answer(X[j]) << ", ";
+        }
+        cout << endl;
+    }
+}
+
+void RungeRomberg(double x0,double x1, double h0, double h1, vector<double>& Y, vector<double>& Z, double y0, double z0)
+{
+    vector<double> X1;
+    for (double i = x0; i <= x1; i = i + h0)
+    {
+        X1.push_back(i);
+    }
+    vector<double> X2;
+    for (double j = x0; j <= x1; j = j + h1)
+    {
+        X2.push_back(j);
+    }
+    cout << "\n" << endl;
+    cout << "оценка Рунге-Ромберга" << endl;
+    vector<double> E1Y1(X1.size()), E2Y1(X1.size()), RY1(X1.size()), AY1(X1.size());
+    Eiler(0, E1Y1, X1, h0, y0, z0,0);
+    Eiler(1, E2Y1, X1, h0, y0, z0,0);
+    Runge(X1, RY1, Z, h0, y0, z0,0,X1.size());
+    Adams(X1, AY1, Z, h0, y0, z0,0);
+
+    vector<double> E1Y2(X2.size()), E2Y2(X2.size()), RY2(X2.size()), AY2(X2.size());
+    Eiler(0, E1Y2, X1, h1, y0, z0, 0);
+    Eiler(1, E2Y2, X1, h1, y0, z0, 0);
+    Runge(X1, RY2, Z, h1, y0, z0, 0, X1.size());
+    Adams(X1, AY2, Z, h1, y0, z0, 0);
+    
+    cout << "Для метода Эйлера: ";
+    for (int i = 0; i < E1Y1.size(); i++) 
+    {
+        cout << E1Y2[i] + (E1Y2[i] - E1Y1[i]) / 3.0<<", ";
+    }
+    cout << endl;
+    cout << "Для Неявного метода Эйлера: ";
+    for (int i = 0; i < E1Y1.size(); i++)
+    {
+        cout << E2Y2[i] + (E2Y2[i] - E2Y1[i]) / 3.0 << ", ";
+    }
+    cout << endl;
+    cout << "Для метода Рунге-Кутты: ";
+    for (int i = 0; i < E1Y1.size(); i++)
+    {
+        cout << RY2[i] + (RY2[i] - RY1[i]) / 3.0 << ", ";
+    }
+    cout << endl;
+    cout << "Для метода Aдамса: ";
+    for (int i = 0; i < E1Y1.size(); i++)
+    {
+        cout << AY2[i] + (AY2[i] - AY1[i]) / 3.0 << ", ";
+    }
+    cout << endl;
+}
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    double h = 0.1;
+    double x0 = 0.0;
+    double x1 = 1.0;
+    double y0 = 1.0;
+    double z0 = 1.0;
+    vector<double> X;
+    for (double i = x0; i <= x1; i = i + h)
+    {
+        X.push_back(i);
+    }
+    vector<double> Y(X.size());
+    vector<double> Z(X.size());
+    Eiler(0,Y, X, h, y0, z0,1);
+    Eiler(1,Y, X, h, y0, z0,1);
+    Runge(X, Y, Z, h, y0, z0,1, X.size());
+    Adams(X, Y, Z, h, y0, z0,1);
+    RungeRomberg(x0, x1, h, 0.01, Y, Z, y0, z0);
+    return 0;
+}
